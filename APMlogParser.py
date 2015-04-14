a227b0f03918cf11d2dae6b95c8448441cfa4c24
jburdy <julien.burdy@gmail.com>
2015-04-14 10:26:54 +0000

    commit initial

---
+++ APMlogParser.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def Parse(logBinFilePath):
+
+    return [('', {})]
+    
+    
+    
+if __name__ == "__main__":
+    # Pour les tests...
+    import sys
+    print Parse(sys.argv[1])
