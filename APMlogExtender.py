d7eda0ed6d391c0b5ffdd825e2866d38da854d1b
jburdy <julien.burdy@gmail.com>
2015-04-14 14:22:46 +0000

    format dico

--- APMlogExtender.py
+++ APMlogExtender.py
@@ -7,57 +7,61 @@ from dateutil import tz
 tick = '0'
 dt = 0
  
+# https://github.com/diydrones/ardupilot/tree/master/ArduCopter
+# https://github.com/diydrones/ardupilot/blob/master/ArduCopter/defines.h
 # http://copter.ardupilot.com/wiki/common-diagnosing-problems-using-logs/#unexpected_errors_including_failsafes
+refErrors = {
+5:{
+	'Subsys': 'Throttle failsafe',
+	'ECode':{
+		1:'throttle dropped below FS_THR_VALUE meaning likely loss of contact between RX/TX',
+		0:'RX/TX contact likely restored'}},
+11:{
+	'Subsys': 'GPS',
+	'ECode':{
+		2:'GPS Glitch',
+		0:'GPS Glitch cleared'}},
+16:{
+	'Subsys': 'EKF/InertialNav Check',
+	'ECode':{
+		2:'Bad Variance',
+		0:'Bad Variance cleared'}},
+17:{
+	'Subsys': 'EKF/InertialNav Failsafe',
+	'ECode':{
+		2:'EKF Failsafe triggered',
+		0:'?????????????????????????????',
+		1:'?????????????????????????????'}}
+}
+
 def printERR(msg):
     global tick, dt
-    print dt, tick, 'ERR -',
-    if msg[1][0] == 'Subsys=5':
-        print 'Throttle failsafe :',
-        if msg[1][1] == 'ECode=1': print 'throttle dropped below FS_THR_VALUE meaning likely loss of contact between RX/TX',
-        if msg[1][1] == 'ECode=0': print 'RX/TX contact likely restored',
-    if msg[1][0] == 'Subsys=11':
-        print 'GPS :',
-        if msg[1][1] == 'ECode=0': print 'GPS Glitch cleared',
-        if msg[1][1] == 'ECode=2': print 'GPS Glitch',
-    if msg[1][0] == 'Subsys=16':
-        print 'EKF/InertialNav Check :',
-        if msg[1][1] == 'ECode=0': print 'Bad Variance cleared',
-        if msg[1][1] == 'ECode=2': print 'Bad Variance',
-    if msg[1][0] == 'Subsys=17':
-        print 'EKF/InertialNav Failsafe :',
-        if msg[1][1] == 'ECode=2': print 'EKF Failsafe triggered',
-    print '-', msg[1]
+    print dt, tick, '- ERR -',
+    try:
+        print refErrors[msg['Subsys']]['Subsys'],
+        print refErrors[msg['Subsys']]['ECode'][msg['ECode']]
+    except:
+        print '\n***************', msg, refErrors[msg['Subsys']]
  
+refModeName = {0: 'Stabilize', 1: 'Acro', 2: 'AltHold', 3: 'Auto', 4: 'Guided', 5: 'PosHold', 6: 'RTL', 7: 'Circle', 8: 'Position', 9: 'Land', 10: 'OF_Loiter'}
+
 def printMODE(msg):
     global tick, dt
-    print dt, tick, 'Mode -',
-    if msg[1][0] == 'Mode=0': print 'Stabilize',
-    if msg[1][0] == 'Mode=1': print 'Acro',
-    if msg[1][0] == 'Mode=2': print 'AltHold',
-    if msg[1][0] == 'Mode=3': print 'Auto',
-    if msg[1][0] == 'Mode=4': print 'Guided',
-    if msg[1][0] == 'Mode=5': print 'PosHold',
-    if msg[1][0] == 'Mode=6': print 'RTL',
-    if msg[1][0] == 'Mode=7': print 'Circle',
-    if msg[1][0] == 'Mode=8': print 'Position',
-    if msg[1][0] == 'Mode=9': print 'Land',
-    if msg[1][0] == 'Mode=10': print 'OF_Loiter',
-    print '-', msg[1]
+    print dt, tick, '- Mode -',
+    print refModeName[msg['Mode']],
+    print '-', msg
  
 # http://ardupilot.com/forum/viewtopic.php?f=80&t=11945
 def parseGPSDate(GPSweek, GPSms):
     return datetime(1980, 1, 6) + timedelta(weeks=GPSweek, hours=2, seconds=(GPSms/1000)-16)
- 
-def getValInt(v):
-    return int(v.split('=')[1])
      
 def printMsg(msg):
     global tick
     global dt
-    if msg[0] == 'ERR': printERR(msg)
-    if msg[0] == 'MODE': printMODE(msg)
-    if msg[0] == 'CURR': tick = str(float(msg[1][0].split('=')[1])/60000.0)[:6]
-    if msg[0] == 'GPS': dt = parseGPSDate(getValInt(msg[1][2]), getValInt(msg[1][1]))
+    if msg[0] == 'ERR': printERR(msg[1])
+    if msg[0] == 'MODE': printMODE(msg[1])
+    if msg[0] == 'CURR': tick = str(float(msg[1]['TimeMS'])/60000.0)[:6]
+    if msg[0] == 'GPS': dt = parseGPSDate(msg[1]['Week'], msg[1]['TimeMS'])
     #print msg
  
  
